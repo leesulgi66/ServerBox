0410ae35cd0313e6a3dc3e52b77deec1b4937593
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-22 16:19:53 +0900

    Initial commit

---
+++ src/main/java/com/example/ServerBox/ServerBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServerBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBoxApplication.class, args);
+	}
+
+}
